9e9ea9a0f6fb3e102468bfcf3164bb7976470a5e
anuragyadav20062002 <anuragyadav20062002@gmail.com>
2021-09-01 21:56:48 +0530

    added minute hand and hour hand js- completed project

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 var secondHand= document.querySelector('.second-hand')
+var minuteHand= document.querySelector('.min-hand')
+var hourHand= document.querySelector('.hour-hand');
 
 
 function setDate(){
 var now = new Date();
 var sec= now.getSeconds()
-var secondsDegree= ((sec/60)*360);
+var secondsDegree= ((sec/60)*360)+90;
 secondHand.style.transform= `rotate(${secondsDegree}deg)`;
-console.log(sec);
+
+var min= now.getMinutes();
+var minuteDegree= ((min/60)*360)+90;
+minuteHand.style.transform=`rotate(${minuteDegree}deg)`;
+
+var hour= now.getHours();
+var hourDegree= ((hour/12)*360)+90;
+minuteHand.style.transform=`rotate(${hourDegree}deg)`;
 }
 
 setInterval(setDate,1000);
